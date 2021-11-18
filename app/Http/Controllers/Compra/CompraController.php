9cc7fa8cf58703ea7b542c072bf21e00fd8082c5
Guido <gmauriciocd@gmail.com>
2021-11-18 18:00:20 -0400

    iniciando configuracion del controlador de compras

---
+++ app/Http/Controllers/Compra/CompraController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Compra;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Compra\Compra;
+
+class CompraController extends Controller
+{
+    //Listado de los recusos
+    public function index(){
+        
+    }
+
+    //Formulario de creacion
+    public function create(){
+      
+    }
+
+    //guardado del formulario
+    public function store(Request $request){
+        
+    }
+
+    //vista de un recurso en especifico
+    public function show(Compra $usuario){
+       
+    }
+
+    //formulario de act
+    public function edit(Compra $usuario){
+       
+    }
+
+    //actualizacion del recurso
+    public function update(Request $request){
+       
+    }
+
+    //eliminar de recurso
+    public function destroy(Request $request){
+
+    }
+}
